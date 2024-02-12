d0525887cde77c561a8f658338c436b086b50adb
Sura Atta <96949252+SuraAtta@users.noreply.github.com>
2024-02-12 19:46:54 +0300

    add the array

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // Using Math.min() to find the youngest students and find() method to return the name
 function showYoungestStudent(students) {
-    var min = Math.min(...students.map(item => item.age));
-    var minStudent = students.find(item => item.age === min);
-    var nameOfStudent = minStudent.name;
-    return nameOfStudent + " is the youngest student.";
+    if (students.length === 0){
+        return "No students provided.";
+    } else{
+        var min = Math.min(...students.map(item => item.age));
+        var minStudent = students.find(item => item.age === min);
+        var nameOfStudent = minStudent.name;
+        return nameOfStudent + " is the youngest student.";
+    }
 }
 
-var students = [{ name: "Sura", age: 24 }, { name: "Noor", age: 20 }, { name: "Hajer", age: 22 }];
-
-console.log(showYoungestStudent(students));
+userDefinedStudents = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 2 },
+    { name: 'Charlie', age: 21 },
+];
+console.log(showYoungestStudent(userDefinedStudents));
 
 // Using for loop and if statment to find the youngest students
 function showYoungestStudent2(students) {
     var temp = "";
+    if (students.length === 0){
+        return "No students provided.";
+    } else {
     for (var i = 0; i < students.length - 1; i++) {
         if (students[i].age < students[i + 1].age) {
             temp = students[i].name
@@ -20,7 +30,8 @@ function showYoungestStudent2(students) {
     }
     return temp + " is the youngest student.";
 }
-console.log(showYoungestStudent2(students));
+}
+console.log(showYoungestStudent2(userDefinedStudents));
 
 module.exports = showYoungestStudent;
 module.exports = showYoungestStudent2;
